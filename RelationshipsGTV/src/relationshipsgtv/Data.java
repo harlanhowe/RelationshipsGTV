ff2952b73a4bd42b54224c0888545cf05773fdaf
george.barrow <george.barrow@172.16.218.213>
2013-11-21 11:24:20 -0600

    Started the addRelationship() function

--- RelationshipsGTV/src/relationshipsgtv/Data.java
+++ RelationshipsGTV/src/relationshipsgtv/Data.java
@@ -42,12 +42,12 @@ public class Data
     }
     
     /**
-     * 
+     * This adds a new relationship to the relationships HashMap.
      * @param personToAdd X has a brother Y, with this person being X. This will be the key in the relationships HashMap.
      * @param relationshipTypeID This is the ID of the desired relationshipType. To get this, call the relationshipType's getID() function.
      * @param secondPerson X has a brother Y, with this person being Y.
      */
-    public void addRelationship(Person personToAdd, int relationshipTypeID, int secondPerson)
+    public void addRelationship(Person firstPerson, RelationshipType relationshipTypeToAdd, Person secondPerson)
     {
         
     }
