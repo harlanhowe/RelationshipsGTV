7cbaa85d6ca1cbc70eddce4bb60430f685b8fe20
george.barrow <george.barrow@172.16.218.213>
2013-11-26 11:43:28 -0600

    added getPeople and getRelationshipTypes stubs

--- RelationshipsGTV/src/relationshipsgtv/Data.java
+++ RelationshipsGTV/src/relationshipsgtv/Data.java
@@ -14,13 +14,13 @@ import java.util.HashMap;
 public class Data 
 {
     public HashMap people, relationshipTypes, relationships;
-    private int peopleID, relationshipTypesID;
+    private int peopleID, relationshipTypesID, relationshipsID;
     
     public Data()
     {
         people = new HashMap<Integer, Person>();
         relationshipTypes = new HashMap<Integer, RelationshipType>();
-        relationships = new HashMap<Person, ArrayList<Integer>>();
+        relationships = new HashMap<Integer, ArrayList<Integer>>();
         peopleID = 1;
         relationshipTypesID = 1;
     }
@@ -32,6 +32,10 @@ public class Data
     // Adding things to the maps
     // --------
     
+    /**
+     * This adds a new Person to the People HashMap.
+     * @param newPerson 
+     */
     public void addToPeople(Person newPerson)
     {
         people.put(peopleID, newPerson);
@@ -39,6 +43,10 @@ public class Data
         peopleID ++;
     }
     
+    /**
+     * This adds a new RelationshipType to the RelationshipTypes HashMap.
+     * @param newRelationshipType 
+     */
     public void addToRelationshipTypes(RelationshipType newRelationshipType)
     {
         relationshipTypes.put(relationshipTypesID, newRelationshipType);
@@ -48,29 +56,23 @@ public class Data
     
     /**
      * This adds a new relationship to the relationships HashMap.
-     * If there is already an entry in the relationships HashMap with the firstPerson key, then it will add the relationship to the existing ArrayList object for that key.
      * @param firstPerson X has a brother Y, with this person being X. This will be the key in the relationships HashMap.
      * @param relationshipTypeToAdd This is the desired relationshipType.
      * @param secondPerson X has a brother Y, with this person being Y.
      */
     public void addRelationship(Person firstPerson, RelationshipType relationshipTypeToAdd, Person secondPerson)
     {
+        int firstID = firstPerson.getID();
         int relTypeID = relationshipTypeToAdd.getID();
         int secondID = secondPerson.getID();
         
-        if (relationships.containsKey(firstPerson))
-        {
-            ArrayList existingList = (ArrayList)(relationships.get(firstPerson));
-            existingList.add(relTypeID);
-            existingList.add(secondPerson);
-        }
-        else
-        {
-            ArrayList relAndPerson2 = new ArrayList<Integer>();
-            relAndPerson2.add(relTypeID);
-            relAndPerson2.add(secondID);
-            relationships.put(firstPerson, relAndPerson2);
-        }
+        ArrayList entry = new ArrayList<Integer>();
+        entry.add(firstID);
+        entry.add(relTypeID);
+        entry.add(secondID);
+        
+        relationships.put(relationshipsID, entry);
+        relationshipsID ++;
     }
     
     
@@ -80,15 +82,14 @@ public class Data
     // Returning data from maps
     // ---------------
     
-//    public Person getPerson(int givenPersonID)
-//    {
-//        Person desiredPerson = (Person)(people.get(givenPersonID));
-//        return desiredPerson;
-//    }
-//    
-//    public RelationshipType getRelationshipType(int givenRelationshipTypeID)
-//    {
-//        RelationshipType desiredRelationshipType = (RelationshipType)(relationshipTypes.get(givenRelationshipTypeID));
-//        return desiredRelationshipType;
-//    }
+    public ArrayList<Person> getPeople(int givenPersonID)
+    {
+        return null;
+        
+    }
+    
+    public RelationshipType getRelationshipTypes(int givenRelationshipTypeID)
+    {
+        return null;
+    }
 }
