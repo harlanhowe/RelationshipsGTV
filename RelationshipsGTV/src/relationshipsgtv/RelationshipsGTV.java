b95dd13db6111a7db9afdbfd55351eac168f4f45
george.barrow <george.barrow@172.16.218.213>
2013-11-26 11:49:44 -0600

    Started writing the save function

--- RelationshipsGTV/src/relationshipsgtv/RelationshipsGTV.java
+++ RelationshipsGTV/src/relationshipsgtv/RelationshipsGTV.java
@@ -4,6 +4,11 @@
  */
 package relationshipsgtv;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author kadmin
@@ -13,10 +18,41 @@ public class RelationshipsGTV {
     /**
      * @param args the command line arguments
      */
+    private File preferredFile;
+    
+    
     public static void main(String[] args) 
     {
         // TODO code application logic here
         //thing    
+        
+    }
     
+    
+    public void save()
+    {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setSelectedFile(preferredFile);
+        int result = chooser.showSaveDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) // if the user clicked "save"
+        {
+            preferredFile = chooser.getSelectedFile();
+            try
+            {
+                PrintWriter output = new PrintWriter(preferredFile);
+                // you can now write to the file by saying
+                // output.println("Here is example stuff to save....");
+                // put your output code here!
+                
+                
+                
+                
+                output.close();
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                throw new RuntimeException("File "+preferredFile+" cannot be saved.");
+            }
+        }
     }
 }
