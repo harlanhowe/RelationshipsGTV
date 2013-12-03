5af3f5c03f941a8f55ad64042e09a317f6c8fb19
tareck.haykal <tareck.haykal@172.16.219.23>
2013-12-03 11:43:19 -0600

    added last name

--- RelationshipsGTV/src/relationshipsgtv/Person.java
+++ RelationshipsGTV/src/relationshipsgtv/Person.java
@@ -20,6 +20,10 @@ public class Person
         lastName=newLastName;
         male = isMale;
     }
+    public String toString()
+    {   
+    return firstName+""+lastName;
+    }
     
     public String getFirstName()
     {
